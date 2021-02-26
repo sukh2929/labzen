e96d022888efdc1d732ade28c13cf938c955f5fd
Kamal Moravej <70611780+kmoravej@users.noreply.github.com>
2021-02-25 19:05:36 -0500

    Add skeleton for functions and fix Github action (#12)

---
+++ mdspyutils/mdspyutils.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def parse_lab(file_name: str) -> list:
+    """[summary]
+
+    Args:
+        file_name (str): [description]
+
+    Returns:
+        list: [description]
+    """
+    return None
+
+
+def count_points(file_name: str) -> pd.DataFrame:
+    """[summary]
+
+    Args:
+        file_name (str): [description]
+
+    Returns:
+        pd.DataFrame: [description]
+    """
+    return None
+
+
+def check_mechanics(file_name: str) -> bool:
+    """[summary]
+
+    Args:
+        file_name (str): [description]
+
+    Returns:
+        bool: [description]
+    """
+    return None
