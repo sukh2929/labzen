ad3ef644fe98f13a2476e566fe66dc32941d1b99
Kamal Moravej <70611780+kmoravej@users.noreply.github.com>
2021-03-05 21:01:19 -0500

    Add tests for parse_lab with Rmd file (#31)

--- tests/test_labzen.py
+++ tests/test_labzen.py
@@ -9,19 +9,35 @@ def test_version():
 
 
 def test_parse_lab():
-    # Define to test files
-    pyfile = Path("data-raw/dummylab.ipynb")
-    rfile = Path("data-raw/dummylab.Rmd")
 
-    assert type(lz.parse_lab("data-raw/dummylab.ipynb")[0]) == type(
-        "# DSCI 563 - Unsupervised Learning"
-    )
-    assert lz.parse_lab("data-raw/dummylab.ipynb")[4] == '## Imports <a name="im"></a>'
-    assert len(lz.parse_lab("data-raw/dummylab.ipynb")) == 24
+    # Define to test files
+    pyfile = "data-raw/dummylab.ipynb"
+    rfile = "data-raw/dummylab.Rmd"
+
+    parsed_lab_py = lz.parse_lab(pyfile)
+    parsed_lab_r = lz.parse_lab(rfile)
+
+    # Test that the return type is list
+    assert type(parsed_lab_py).__name__ == "list"
+    assert type(parsed_lab_r).__name__ == "list"
+
+    # Test that the elements of list is string
+    assert type(parsed_lab_py[0]).__name__ == "str"
+    assert type(parsed_lab_r[0]).__name__ == "str"
+
+    # Test if the right cell type parsed
+    assert parsed_lab_py[4] == '## Imports <a name="im"></a>'
+    assert len(parsed_lab_r[2]) == 760
+
+    # Test if all markdown cells has been parsed
+    assert len(parsed_lab_py) == 24
+    assert len(parsed_lab_r) == 4
+
+    # Test if the function raise exception for a wrong input
     with pytest.raises(Exception):
         lz.parse_lab("lab1.csv")
     with pytest.raises(Exception):
-        lz.parse_lab(4)
+        lz.parse_lab(data - raw / dummylab.ipynb)
 
 
 def test_count_points():
